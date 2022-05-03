0792dea8c1a51d3332231613171b37fdd5a2728c
Alex <agallardo@ibadia.cat>
2022-05-03 17:17:31 +0200

    Start Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+import * as handPoseDetection from '@tensorflow-models/hand-pose-detection';
+
+const model = handPoseDetection.SupportedModels.MediaPipeHands;
+const detectorConfig = {
+  runtime: 'mediapipe', // or 'tfjs'
+  modelType: 'full'
+};
+detector = await handPoseDetection.createDetector(model, detectorConfig);
+
+const video = document.getElementById('video');
+const hands = await detector.estimateHands(video);
